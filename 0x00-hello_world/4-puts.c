50c9265665fd133fe0904acc7ea38156d45ac5d7
Yididya G. Abraha <yidu95@gmail.com>
2022-06-23 23:08:36 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <sys/syscall.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
  */
 int main(void)
 {
-	puts();
-	return (0);
-}
-
-
-int puts(void)
-{
-	const char msg[] = "Programming is like building a multilingual puzzle";
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	char msg[] = "Programming is like building a multilingual puzzle";
+	puts(msg);
 	return (0);
 }
