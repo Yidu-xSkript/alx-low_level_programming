f284b594c8437968c5db363dedf1afaebce1f65c
Yididya G. Abraha <yidu95@gmail.com>
2022-06-25 03:47:18 -0700

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,16 @@
 /**
  * main - Entry point
  *
+ * Description: 'Hello, puts'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char msg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(msg);
+
 	return (0);
 }
