e672d9461685648ba0bcb62103b2f4d30673bcbb
Yididya G. Abraha <yidu95@gmail.com>
2022-06-25 23:06:16 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'UNIX is basically a simple OS, but you have to be a genius to understand the simplicity'
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+ 	return (1);
+}
