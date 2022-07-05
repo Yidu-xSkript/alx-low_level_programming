82e564527519504720d8de873138d476b330567c
Yidu <yidu95@gmail.com>
2022-07-05 12:25:48 +0300

    1-alphabetfix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * print_alphabet - Print alphabet
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char ch;
 
