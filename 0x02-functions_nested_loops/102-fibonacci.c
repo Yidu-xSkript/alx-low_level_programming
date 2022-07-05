69012b27dfa933b56d097d62ca3a3426ca59c238
Yidu <yidu95@gmail.com>
2022-07-05 16:21:29 +0300

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,12 +6,12 @@
  **/
 int main(void)
 {
-	int f1 = 0, f2 = 1, i;
+	int f1 = 0, f2 = 1, i, next;
 
 	for (i = 1; i <= 50; i++)
 	{
 		printf("%d ", f2);
-		int next = f1 + f2;
+		next = f1 + f2;
 		f1 = f2;
 		f2 = next;
 	}
