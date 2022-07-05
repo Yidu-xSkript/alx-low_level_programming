b23a155b1d0085a1ed74f4c89bed57a3d7c94542
Yidu <yidu95@gmail.com>
2022-07-05 13:00:41 +0300

    LOL: FORGOT semicolons

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
  * print_sign - Print Sign
- *
  * Description: Print Sign
- *
  * @n: integer to check
- *
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+')
-		return (1)
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0')
-		return (0)
+		_putchar('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-')
-		return (-1)
+		_putchar('-');
+		return (-1);
 	}
 }
