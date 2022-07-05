a1e19271f877e93eac5c1e5199d212b0c379cda7
Yidu <yidu95@gmail.com>
2022-07-05 17:23:52 +0300

    advanced fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Description: Fibonacci
+ * Return: Always 0.
+ **/
+int main(void)
+{
+	int i = 2;
+	printf("%d", sum(i));
+	return (0);
+}
+
+/**
+ * sum - Sum function
+ * Description: Fibonacci
+ * @n: int input
+ * Return: int sum.
+ **/
+int sum(int n) {
+	int sum = 0;
+	while (fib(n) < N)
+	{
+		if (fib(n)%2==0)
+			sum += fib(n);
+		n++;
+	}
+	return sum;
+}
+
+/**
+ * fib - Fib function
+ * Description: Fibonacci
+ * @n: int input
+ * Return: int c.
+ **/
+int fib(int n) {
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	int i = 1;
+
+	while (i <= n)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		i++;
+	}
+	return c;
+}
