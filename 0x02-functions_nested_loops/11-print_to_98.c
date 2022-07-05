d2b4c566815be4a39215c95ce1ba1aea171210c7
Yidu <yidu95@gmail.com>
2022-07-05 12:13:33 +0300

    98 bettery st. th og

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - Print to 98 from every dxn
+ * @n: number
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		_putchar(n);
+		if (n > 98)
+			n--;
+		else if (n < 98)
+			n++;
+	}
+	if (n == 98)
+		_putchar(n);
+}
