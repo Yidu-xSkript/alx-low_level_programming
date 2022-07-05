c1bfa6212eba26d5834e8e0d8b2c7641da72e841
Yidu <yidu95@gmail.com>
2022-07-05 14:58:41 +0300

    advanced times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* print_times_table - prints times table
+* Description: Prints times table
+* @n: int input
+**/
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n < 15 && n > 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (y != 0)
+				{
+					printf(", ");
+				}
+				if (y == 0)
+				{
+					printf("0");
+				}
+				else if (z >= 10)
+				{
+					printf("%d%d", (z / 10), (z % 10));
+				}
+				else if ((z < 10) && (y != 0))
+				{
+					printf(" %d", (z % 10));
+				}
+			}
+			printf("\n");
+		}
+	}
+}
