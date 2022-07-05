322f31c887c0c3ac2650524666d89cb953113a0a
Yidu <yidu95@gmail.com>
2022-07-05 13:19:25 +0300

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,11 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-			_putchar(hours);
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
-			_putchar(minutes);
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 			minutes++;
 		}
