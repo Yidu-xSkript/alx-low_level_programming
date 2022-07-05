e2844ca5d1ec624f1c448d8b00cb727413de1f40
Yidu <yidu95@gmail.com>
2022-07-05 17:47:39 +0300

    advanced fib II

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,15 @@
  **/
 int main(void)
 {
-	int64_t f1 = 0, f2 = 1, i;
-	int64_t next = f1 + f2;
+	uint64_t f1 = 0, f2 = 1, i;
+	uint64_t next = f1 + f2;
 
 	for (i = 1; i <= 98; ++i)
 	{
 		if (i < 98)
-			printf("%" PRId64 ", ", next);
+			printf("%" PRIu64 ", ", next);
 		else
-			printf("%" PRId64 "\n", next);
+			printf("%" PRIu64 "\n", next);
 		f1 = f2;
 		f2 = next;
 		next = f1 + f2;
