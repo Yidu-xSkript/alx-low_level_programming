9fa248b4c8bfe61e2292ffa538700ac3440d666d
Yidu <yidu95@gmail.com>
2022-07-05 18:33:19 +0300

    advanced fib II

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,107 +1,21 @@
 #include <stdio.h>
-#include "main.h"
-
-typedef unsigned char byte;
-#define N 300;
-
-typedef struct
-{
-	byte digits[N];
-} Bignum;
-
-/**
- * printbig - Entry Point
- * Description: Fibonacci
- * @a: Bignum
- **/
-
-void printbig(Bignum a)
-{
-	int leading = 1;
-
-	for (int i = 0; i < N; ++i)
-	{
-		if (a.digits[i])
-		{
-			leading = 0;
-			printf("%c", a.digits[i] + '0');
-		}
-		else
-		{
-			if (!leading)
-			{
-				printf("0");
-			}
-		}
-	}
-	if (leading)
-		printf("0");
-	puts("");
-}
-
-/**
- * initbig - Entry Point
- * Description: Fibonacci
- * @x: int
- * Return: int a.
- **/
-
-Bignum initbig(int x)
-{
-	Bignum a = {{0}};
-
-	a.digits[N - 1] = x;
-	return (a);
-}
-
-/**
- * addbig - Entry Point
- * Description: Fibonacci
- * @a: Bignum
- * @b: Bignum
- * Return: Bignum c.
- **/
-
-Bignum addbig(Bignum a, Bignum b)
-{
-	Bignum c;
-	int sum, carry = 0;
-
-	for (int i = N - 1; i >= 0; --i)
-	{
-		sum = a.digits[i] + b.digits[i] + carry;
-
-		if (sum <= 9)
-		{
-			c.digits[i] = sum;
-			carry = 0;
-		}
-		else
-		{
-			c.digits[i] = sum - 10;
-			carry = 1;
-		}
-	}
-	return (c);
-}
-
+#include <inttypes.h>
 /**
  * main - Entry Point
  * Description: Fibonacci
  * Return: Always 0.
  **/
-
 int main(void)
 {
-	Bignum f1 = 0, f2 = 1;
-	int i;
-	Bignum next = addbig(f1, f2);
+	long long int f1 = 0, f2 = 1, i;
+	long long int next = f1 + f2;
 
 	for (i = 1; i <= 98; ++i)
 	{
 		if (i < 98)
-			printbig("%d, ", next);
-
+			printf("%" PRIu64 ", ", next);
+		else
+			printf("%" PRIu64 "\n", next);
 		f1 = f2;
 		f2 = next;
 		next = f1 + f2;
