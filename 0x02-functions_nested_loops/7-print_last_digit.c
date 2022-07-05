3da84ce73cb8b0397e98581866263808fbdf1b95
Yidu <yidu95@gmail.com>
2022-07-05 13:09:39 +0300

    print last digit fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,8 @@
+#include "main.h"
 /**
  * print_last_digit - Print the last digit
- *
  * Description: Print the last digit
- *
  * @j: number | integer
- *
  * Return: int (Success)
  */
 
@@ -13,5 +11,6 @@ int print_last_digit(int j)
 	j = j % 10;
 	if (j < 0)
 		j = -j;
+   	 _putchar(j + '0');
 	return (j);
 }
