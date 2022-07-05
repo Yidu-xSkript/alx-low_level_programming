856cdabd763bdfe6c2ef6406a726ad5d282b64b4
Yidu <yidu95@gmail.com>
2022-07-05 11:33:28 +0300

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Print Sign
+ *
+ * Description: Print Sign
+ *
+ * @n: integer to check
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+')
+		return (1)
+	}
+	else if (n == 0)
+	{
+		_putchar('0')
+		return (0)
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1)
+	}
+}
