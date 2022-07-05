17654d2728772aa9fd333522dffdfc4ec9a67f27
Yidu <yidu95@gmail.com>
2022-07-05 15:35:28 +0300

    modified for better use

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,20 @@ void print_times_table(int n)
 	int x, y, z;
 
 	if (n < 15 && n > 0)
-	{
 		for (x = 0; x <= n; x++)
 		{
 			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
 				if (y != 0)
-				{
 					printf(",");
-				}
 				if (y == 0)
-				{
 					printf("0");
-				}
 				else if (z >= 10)
-				{
-					printf("%3d", (z / 10));
-          printf("%d", (z % 10));
-				}
+					printf("%3d%d", (z / 10), (z % 10));
 				else if ((z < 10) && (y != 0))
-				{
 					printf("%4d", (z % 10));
-				}
 			}
 			printf("\n");
 		}
-	}
 }
