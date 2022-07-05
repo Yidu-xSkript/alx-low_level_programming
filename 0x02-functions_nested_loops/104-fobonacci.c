68bcda3d4b419d2655e635ebbf9df702b48b0ec5
Yidu <yidu95@gmail.com>
2022-07-05 17:34:32 +0300

    advanced fib II

---
+++ 0x02-functions_nested_loops/104-fobonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Fibonacci
+ * Return: Always 0.
+ **/
+int main(void)
+{
+	long f1 = 0, f2 = 1, i;
+	long next = f1 + f2;
+
+	for (i = 1; i <= 98; ++i)
+	{
+		if (i < 98)
+			printf("%ld, ", next);
+		else
+			printf("%ld\n", next);
+		f1 = f2;
+		f2 = next;
+		next = f1 + f2;
+	}
+	return (0);
+}
