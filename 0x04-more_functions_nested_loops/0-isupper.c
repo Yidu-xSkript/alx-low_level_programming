be8ecb32c1326b806c072a74853b1ac35cb633d7
Yidu-xSkript <yidu95@gmail.com>
2022-07-07 01:24:25 -0700

    is digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
 #include <ctype.h>
 
-/*
+/**
 * _isupper - Check uppercase values
 * Description: Upper case values test
+* @c: int input
 * Result: 0 or 1 (Success)
 */
+
 int _isupper(int c)
 {
     if (isupper(c))
