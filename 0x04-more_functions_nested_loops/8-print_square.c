0c5fdda6afd17558d90801a2fe92caa3202fb6e6
Yidu-xSkript <yidu95@gmail.com>
2022-07-07 03:02:16 -0700

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Print square
+ * description: Print square based on size.
+ * @size: int input
+ */
+
+void print_square(int size)
+{
+	int i, j = 0;
+
+	while (j < size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		j++;
+	}
+}
