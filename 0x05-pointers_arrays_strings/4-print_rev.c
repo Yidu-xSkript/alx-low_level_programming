a2f6da18e8e1516183c552ec140279fe3e972f96
Yididya G. Abraha <yidu95@gmail.com>
2022-07-11 08:03:46 -0700

    code fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,12 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
-	while (*s != '\0') {
-		++length;
-		++s;
-	}
+	while (s[i++])
+		length++;
 
-	for (i = length; i >= 0; --i) {
-		_putchar(*(s));
-		--s;
-	}
+
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
