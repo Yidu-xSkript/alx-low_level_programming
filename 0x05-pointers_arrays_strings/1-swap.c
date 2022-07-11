1fa2953761a0f8e4a6960a9d85dab6a302494af1
Yididya G. Abraha <yidu95@gmail.com>
2022-07-11 07:44:35 -0700

    swap - betty code fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 
 /**
- * swap_int - check the code 
+ * swap_int - check the code
  * @a: pointer int
  * @b: pointer int
  */
 
 void swap_int(int *a, int *b)
 {
-    *a = 42;
-    *b = 98;
+	*a = 42;
+	*b = 98;
 }
