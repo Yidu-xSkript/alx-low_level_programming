a9d8ebcc7210cca85034bfead8e73f6d4ef86203
Yidu-xSkript <yidu95@gmail.com>
2022-07-13 12:31:08 +0300

    0x06-pointers_arr

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - copy 2 strings
+ * @dest:  char
+ * @src: char
+ * @n: int
+ * Return: char* 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
