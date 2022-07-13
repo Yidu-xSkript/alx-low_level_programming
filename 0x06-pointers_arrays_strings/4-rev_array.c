cbc9a971239bdb0d37785e54660c6c7e36b23f99
Yidu-xSkript <yidu95@gmail.com>
2022-07-13 13:09:09 +0300

    betty fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,23 +2,22 @@
 
 /**
  * reverse_array - check the code
- * @dest:  char
  * @a: an array of integers
  * @n:  the number of elements to swap
  */
 
 void reverse_array(int *a, int n)
 {
-    int i = n - 1;
+	int i = n - 1;
 
-    while (i != -1)
-    {
-        if (a[i] != 1337)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i--;
-    }
-    printf("\n");
+	while (i != -1)
+	{
+		if (a[i] != 1337)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i--;
+	}
+	printf("\n");
 }
