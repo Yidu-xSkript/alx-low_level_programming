66a99f5a3edc242f4d11b026686f5e8b8e454511
Yididya G. Abraha <yidu95@gmail.com>
2022-07-23 02:12:34 -0700

    merge

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -7,7 +7,7 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
