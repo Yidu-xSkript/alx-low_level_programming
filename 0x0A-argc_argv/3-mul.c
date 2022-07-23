0e86367e564032a68c6f0e7c317e300b7d36cd83
Yidu-xSkript <yidu95@gmail.com>
2022-07-23 13:20:11 +0300

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main (int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+    return (0);
+}
