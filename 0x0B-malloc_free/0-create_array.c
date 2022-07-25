4495f87c4eb29e225aa16296729dcb190feacafa
Yidu-xSkript <yidu95@gmail.com>
2022-07-25 15:34:43 +0300

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,11 +18,11 @@ char *create_array(unsigned int size, char c)
 
 	ch = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-		ch[i] = c;
-
 	if (ch == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ch[i] = c;
 
 	return (ch);
 }
